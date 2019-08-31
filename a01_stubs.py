20c6e71653a8ecad3da5d5bad33f2b872a56f282
frankb <frankb@berea.edu>
2019-08-31 19:52:24 -0400

    Added Name, Finished Task 1 and 2, and Extra Credit

--- a01_stubs.py
+++ a01_stubs.py
@@ -22,7 +22,7 @@ birth_year1 = int(input("So, what year were your born?"))
 
 ######################################################################
 # This formula takes the year given and converts it into a counter from 0-11
-zodiac_number1= birth_year1 % 12
+zodiac_number1 = birth_year1 % 12
 
 # This assigns the counter to the correct Zodiac animal
 if zodiac_number1 == 0:
@@ -55,33 +55,86 @@ else:
 # The time.sleep helps the program look like its "thinking"
 
 print("Lets see here.")
-time.sleep(2)
+# time.sleep(2)
 print("If you were born then,")
-time.sleep(2)
+# time.sleep(2)
 print("then thaaaat means...")
-time.sleep(2)
+# time.sleep(2)
 print('.')
-time.sleep(1)
+# time.sleep(1)
 print('.')
-time.sleep(1)
+# time.sleep(1)
 print('.')
-time.sleep(1)
+# time.sleep(1)
 print('Ah! So according to the Chinese Zodiac, you are a ' + zodiac_animal1 + '!')
-
+print("That's so cool!")
+# time.sleep(2)
 ######################################################################
-# (Required) Task 2
-# TODO Ask the user for their friend's birth year
+# This section asks for the birth year of a friend
 
+# This will allows them to terminate the program if they don't wish to put in a friend's birth year.
+want_friend = input("So, would you like to see how you match with a friend, " + name + "?")
 
-# TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
-
-
-######################################################################
+# Just like the last counter, this will assign their year a counter and an animal
+if want_friend == "yes" or want_friend == "Yes":
+    birth_year2 = int(input("Sweet! So what is your friend's birth year?"))
+    zodiac_number2 = birth_year2 % 12
+    if zodiac_number2 == 0:
+        zodiac_animal2 = 'Monkey'
+    elif zodiac_number2 == 1:
+        zodiac_animal2 = 'Rooster'
+    elif zodiac_number2 == 2:
+        zodiac_animal2 = 'Dog'
+    elif zodiac_number2 == 3:
+        zodiac_animal2 = 'Pig'
+    elif zodiac_number2 == 4:
+        zodiac_animal2 = 'Rat'
+    elif zodiac_number2 == 5:
+        zodiac_animal2 = 'Ox'
+    elif zodiac_number2 == 6:
+        zodiac_animal2 = 'Tiger'
+    elif zodiac_number2 == 7:
+        zodiac_animal2 = 'Rabbit'
+    elif zodiac_number2 == 8:
+        zodiac_animal2 = 'Dragon'
+    elif zodiac_number2 == 9:
+        zodiac_animal2 = 'Snake'
+    elif zodiac_number2 == 10:
+        zodiac_animal2 = 'Horse'
+    else:
+        zodiac_animal2 = 'Goat'
+# This is to allow for the program to have some personality
+    print("Okay, okay...hmmm...")
+    time.sleep(2)
+    print("Give me a second to figure this out")
+    time.sleep(2)
+    print("So, if you are a " + zodiac_animal1 + ",")
+    time.sleep(1)
+    print('and I see that your friend is a ' + zodiac_animal2 + ',')
+    time.sleep(1)
+    print('soo...')
+    time.sleep(1)
+#########################################################################
 # (Optional) Task 3
-# TODO Check for compatibility between your birth year and your friend's birth year
-# NOTE: You can always assume the first input is your birth year (i.e., 1982 for me).
-# This way, you are not writing a ton of code to consider every possibility.
-# In other words, only do one row of the sample compatibility table.
-
-
-# TODO print if you are a strong match, no match, or in between
+# Here, the program takes the difference between the two years.
+# If they are 0,4, or 8 years apart, they are a match
+# If they are 6 years apart, they are not a match
+    match_coefficient = abs(birth_year1 - birth_year2)
+    if match_coefficient == 0 or match_coefficient == 4 or match_coefficient == 8:
+        print('You and your friend are a match made in heaven!!!')
+        time.sleep(2)
+        print('Now if only I could find a better RAM card...')
+    elif match_coefficient == 6:
+        print('Ouch, looks like your "friend" may not be the best fit with you.')
+        time.sleep(2)
+        print('Though who cares about what everyone else says,')
+        time.sleep(1)
+        print('You know your friend better than I do!')
+    else:
+        print("Seems like you've got an 'okay' friend there, bud!")
+        print('''But remember, "EXTRA"ordinary isn't the same thing as "Extraordinary"''')
+        time.sleep(2)
+###########################################################################
+# This gives a farewell if they didn't want to put in a friend's birth year
+else:
+    print("Well, I hope I was helpful! Have a good day!")
